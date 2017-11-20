fa8cb1b8f0021fbc539ce2d280ec232613e178ea
liwangjun <459378261@qq.com>
2017-11-20 19:24:20 +0800

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,7 @@ Vue.use(Router)
 
 const routes = [{
     path: '/',
-    component: HelloWorld,
+    component: HelloWorld
     // meta: {
     //   requireAuth: true, // 添加该字段，表示进入这个路由是需要登录的
     // },
