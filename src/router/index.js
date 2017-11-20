8ebf1479d63d0d35c3bf5a8219869a9973c37869
liwangjun <459378261@qq.com>
2017-11-20 19:12:51 +0800

    20171120 提交   电话征信

--- src/router/index.js
+++ src/router/index.js
@@ -19,9 +19,10 @@ import SplitScreen from '@/view/FirstTrail/SplitScreen'
 import AudioVisual from '@/view/FirstTrail/detailComponent/AudioVisual'
 import CreditForm from '@/view/FirstTrail/detailComponent/CreditForm'*/
 import login from '@/view/login/login'
-import InternalMatch from '@/view/InternalMatch/InternalMatch'
-import PhoneCredit from '@/view/PhoneCredit/PhoneCredit'
-
+import InternalMatch from '@/view/FirstTrail/InternalMatch/InternalMatch'
+import PhoneCredit from '@/view/FirstTrail/PhoneCredit/PhoneCredit'
+// 住址电话
+import AddressForm from '@/view/FirstTrail/PhoneCredit/Form/AddressForm'
 
 
 Vue.use(Router)
@@ -80,7 +81,14 @@ const routes = [{
   },{
     //电话征信
     path: '/PhoneCredit',
-    component: PhoneCredit
+    component: PhoneCredit,
+    children:[
+      {
+        path:'/AddressForm/:formTag/:id/:phoneType',
+        component: AddressForm,
+
+      }
+    ]
   }
 ];
 
