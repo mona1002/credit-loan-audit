95e2d18de1d584a000b5422a08511423afb54842
yueyanyan <yueyanyan@nuoyuan.com.cn>
2017-12-05 21:07:05 +0800

    借款人信息 数字加小数点

--- src/view/FirstTrail/detailComponent/borrowerInformation.vue
+++ src/view/FirstTrail/detailComponent/borrowerInformation.vue
@@ -806,7 +806,8 @@
 				        label="N"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <input v-model="scope.row.n" type=number></input>
+				        	<input type="text" name="" v-model="scope.row.n" placeholder="请输入内容" v-numbers>
+				          <!-- <el-input v-model="scope.row.n" placeholder="请输入内容" v-numbers></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -814,7 +815,8 @@
 				        label="N-1"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <el-input v-model="scope.row.n1" placeholder="请输入内容"></el-input>
+				        	<input type="text" name="" v-model="scope.row.n1" placeholder="请输入内容" v-numberss>
+						  <!-- <el-input v-model="scope.row.n1" placeholder="请输入内容"></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -822,7 +824,8 @@
 				        label="N-2"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <el-input v-model="scope.row.n2" placeholder="请输入内容"></el-input>
+				        	<input type="text" name="" v-model="scope.row.n2" placeholder="请输入内容" v-numbers>
+						  <!-- <el-input v-model="scope.row.n2" placeholder="请输入内容"></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -830,7 +833,8 @@
 				        label="N-3"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <el-input v-model="scope.row.n3" placeholder="请输入内容"></el-input>
+				        	<input type="text" name="" v-model="scope.row.n3" placeholder="请输入内容" v-numbers>
+						  <!-- <el-input v-model="scope.row.n3" placeholder="请输入内容"></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -838,7 +842,8 @@
 				        label="N-4"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <el-input v-model="scope.row.n4" placeholder="请输入内容"></el-input>
+				        	<input type="text" name="" v-model="scope.row.n4" placeholder="请输入内容" v-numbers>
+						  <!-- <el-input v-model="scope.row.n4" placeholder="请输入内容"></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -846,7 +851,8 @@
 				        label="N-5"
 				        min-width="130">
 				        <template slot-scope="scope">
-						  <el-input v-model="scope.row.n5" placeholder="请输入内容"></el-input>
+				        	<input type="text" name="" v-model="scope.row.n5" placeholder="请输入内容" v-numbers>
+						  <!-- <el-input v-model="scope.row.n5" placeholder="请输入内容"></el-input> -->
 						</template>
 				      </el-table-column>
 				      <el-table-column
@@ -1677,7 +1683,115 @@
 				document.getElementsByTagName('body')[0].style.overflow='';
 				this.layer=false;
 			},
-	    }
+			
+
+	    },
+	    directives: {
+	    // 指令的定义
+	    numbers:{
+      		twoWay: true,
+      		bind:function (el) {
+      			console.log(el);
+				el.addEventListener('blur',function () {
+    				let value;
+    				function formatNumber(num,cent,isThousand) {
+					    var num = num.toString().replace(/\$|\,/g,'');
+
+					    // 检查传入数值为数值类型
+					    if(isNaN(num))
+					        num = "0";
+
+					    // 获取符号(正/负数)
+					    let sign = (num == (num = Math.abs(num)));
+
+					    num = Math.floor(num*Math.pow(10,cent)+0.50000000001);  // 把指定的小数位先转换成整数.多余的小数位四舍五入
+					    let cents = num%Math.pow(10,cent);              // 求出小数位数值
+					    num = Math.floor(num/Math.pow(10,cent)).toString();   // 求出整数位数值
+					    cents = cents.toString();               // 把小数位转换成字符串,以便求小数位长度
+
+					    // 补足小数位到指定的位数
+					    while(cents.length<cent)
+					        cents = "0" + cents;
+
+					    if(isThousand) {
+					        // 对整数部分进行千分位格式化.
+					        for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+					            num = num.substring(0,num.length-(4*i+3))+','+ num.substring(num.length-(4*i+3));
+					    }
+
+					    if (cent > 0)
+					        return (((sign)?'':'-') + num + '.' + cents);
+					    else
+					        return (((sign)?'':'-') + num);
+						};
+    				console.log(formatNumber(el.value,2,0));
+			        (function(){
+			            value = formatNumber(el.value,2,0)
+			           	console.log(value);
+			            return value
+			        })()
+    				el.value =value
+				})
+			},
+	  		update:function (el,binding,vnode) {
+		        if(el.value !== ''){
+		            el.value = el.value.replace(/[^0-9.]+/g, '');
+		        }
+		    }
+		},
+		numberss:{
+      		twoWay: true,
+      		bind:function (el) {
+      			console.log(el);
+				el.addEventListener('blur',function () {
+    				let value;
+    				function formatNumber(num,cent,isThousand) {
+					    var num = num.toString().replace(/\$|\,/g,'');
+
+					    // 检查传入数值为数值类型
+					    if(isNaN(num))
+					        num = "0";
+
+					    // 获取符号(正/负数)
+					    let sign = (num == (num = Math.abs(num)));
+
+					    num = Math.floor(num*Math.pow(10,cent)+0.50000000001);  // 把指定的小数位先转换成整数.多余的小数位四舍五入
+					    let cents = num%Math.pow(10,cent);              // 求出小数位数值
+					    num = Math.floor(num/Math.pow(10,cent)).toString();   // 求出整数位数值
+					    cents = cents.toString();               // 把小数位转换成字符串,以便求小数位长度
+
+					    // 补足小数位到指定的位数
+					    while(cents.length<cent)
+					        cents = "0" + cents;
+
+					    if(isThousand) {
+					        // 对整数部分进行千分位格式化.
+					        for (var i = 0; i < Math.floor((num.length-(1+i))/3); i++)
+					            num = num.substring(0,num.length-(4*i+3))+','+ num.substring(num.length-(4*i+3));
+					    }
+
+					    if (cent > 0)
+					        return (((sign)?'':'-') + num + '.' + cents);
+					    else
+					        return (((sign)?'':'-') + num);
+						};
+    				console.log(formatNumber(el.value,2,0));
+			        (function(){
+			            value = formatNumber(el.value,2,0)
+			           	console.log(value);
+			            return value
+			        })()
+    				el.value =value
+				})
+			},
+	  		update:function (el,binding,vnode) {
+		        if(el.value !== ''){
+		            el.value = el.value.replace(/[^0-9.]+/g, '');
+		        }
+		    }
+		},
+
+		},
 	}
 </script>
 <style type="text/css" scoped>
