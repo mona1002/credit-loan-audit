a850703d9f6d146aa12b286ec91ca59451c2494d
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-07 18:19:33 +0800

    解决冲突

--- src/util/http.js
+++ src/util/http.js
@@ -9,6 +9,9 @@ axios.defaults.timeout = 5000;
 //  axios.defaults.baseURL = 'http://10.1.26.17:8080/riskManagement';//俊波本地地址
 //  axios.defaults.baseURL = 'http://10.1.26.47:8080/riskManagement';//沿海本地地址
 
+// 设置options 不进行发送option 验证  ------错误
+// Vue.http.options.emulateJSON = true
+
 axios.defaults.withCredentials=true;
 
 axios.interceptors.response.use(
