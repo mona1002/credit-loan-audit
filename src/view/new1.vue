2b2776df0f4fed11a40b8a9f69994a85ccdc78dd
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-07 14:28:28 +0800

    解决冲突文件

---
+++ src/view/new1.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="new1">
+<h1>new1</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    mounted() {
+      //  localStorage.setItem("userInf", JSON.stringify(userInf));
+      // var userInf = localStorage.getItem("userInf");
+
+      
+            //   localStorage.setItem("userInf", JSON.stringify(userInf));
+    //  this.localInf=JSON.parse(localStorage.getItem("taskInWaitting") ) ;
+    }
+  }
+
+</script>
+
+<style scoped>
+  .a {
+    white-space: normal;
+    word-break: break-all;
+    /* no break */
+  }
+
+</style>
