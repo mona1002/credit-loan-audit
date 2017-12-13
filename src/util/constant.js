c65645fc1da13724f4340b30f6f5db164200bfee
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-13 15:45:00 +0800

    测试

--- src/util/constant.js
+++ src/util/constant.js
@@ -5,11 +5,11 @@ var appConstant = {};
 // url 配置
 var host = location.hostname,
   port = 80,
-  developmentEnv = "10.1.26.203", // 开发环境
+  developmentEnv = "coderiskmgt.nuoyuan.com.cn", // 开发环境
   // developmentEnv = "10.1.26.50", // 刘礼飞本地环境
-  testEnv = "10.1.26.200", // 测试环境
-  releaseEnv = "10.1.26.204", // 准生产环境
-  productionEnv = "10.1.26.205"; // 生产环境
+  testEnv = "testriskmgt.nuoyuan.com.cn", // 测试环境
+  releaseEnv = "preriskmgt.nuoyuan.com.cn", // 准生产环境
+  productionEnv = "riskmgt.nuoyuan.com.cn"; // 生产环境
 
 if (host === productionEnv) {
   host = productionEnv;
