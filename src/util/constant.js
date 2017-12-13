69e0175ac5636d02e87fa542fae83057fc090ba6
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-13 15:13:33 +0800

    测试

--- src/util/constant.js
+++ src/util/constant.js
@@ -5,7 +5,7 @@ var appConstant = {};
 // url 配置
 var host = location.hostname,
   port = 80,
-  developmentEnv = "10.1.26.200", // 开发环境
+  developmentEnv = "10.1.26.203", // 开发环境
   // developmentEnv = "10.1.26.50", // 刘礼飞本地环境
   testEnv = "10.1.26.200", // 测试环境
   releaseEnv = "10.1.26.200", // 准生产环境
@@ -17,13 +17,14 @@ if (host === productionEnv) {
   host = releaseEnv;
 } else if (host === testEnv) {
   host = testEnv;
+  port = 20716;
 } else {
   host = developmentEnv;
-  port = 20717;
+  port = 20716;
   // port = 8080;
 }
 
-appConstant.baseUrl_user = "http://" + host + (80 === port ? '' : (':' + port)) + "/";
-appConstant.baseUrl_common = "http://" + host + (80 === port ? '' : (':' + port)) + "/";
+// appConstant.baseUrl_user = "http://" + host + (80 === port ? '' : (':' + port)) + "/";
+appConstant.baseUrl_common = "http://" + host + (80 === port ? '' : (':' + port)) + "/riskManagement";
 
 export default appConstant
--- src/util/http.js
+++ src/util/http.js
@@ -1,9 +1,10 @@
 import axios from 'axios' 
 import router from'../router/index'
+import appConstant from './constant'
 // axios 配置
 axios.defaults.timeout = 5000; 
 //数据接口
- axios.defaults.baseURL = 'http://10.1.26.203:20716/riskManagement';
+ axios.defaults.baseURL =appConstant.baseUrl_common;
 
 
 axios.defaults.withCredentials=true;
@@ -21,6 +22,7 @@ axios.interceptors.response.use(
   error => {
       return Promise.reject(error);
   });
+
 
 export default{
     install(Vue,options)
