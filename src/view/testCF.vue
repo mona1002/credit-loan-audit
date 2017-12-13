c96c14eb595f30c8ac8d43a2add452c9359484de
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-13 10:29:18 +0800

    更新路由

---
+++ src/view/testCF.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="testCF">
+<input type="number">
+<el-input  v-model="value" type="number"></el-input>
+  </div>
+</template>
+<script>
+
+export default {
+data(){
+  return{
+value:'',
+  }
+}
+}
+</script>
+<style scoped>
+  .testCF li {
+    /* background: red; */
+    border: 1px solid yellowgreen;
+    margin-top: 30px;
+    height: 40px;
+    line-height: 40px;
+  }
+
+  .testCF li p {
+    float: left;
+    width: 50%;
+  }
+
+  input {
+    border: 1px solid lightblue;
+  }
+
+</style>
