afb2d4079b1f8a866a62ee8e27e6e766ec522ac5
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-14 16:30:47 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -9,19 +9,17 @@ import http from '@/util/http'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import $ from 'jquery'
+import promise from 'es6-promise';
+promise.polyfill();
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI)
 Vue.use(http)
 
-// 设置options 不进行发送option 验证  ------错误用法？
-// Vue.http.options.emulateJSON = true
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  // store,
   template: '<App/>',
   components: { App }
 })
