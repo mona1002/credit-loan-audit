689f9088386b3a7cefd3c2b4b32be095ea6cbfb7
yueyanyan <yueyanyan@nuoyuan.com.cn>
2017-12-18 14:48:44 +0800

    去掉es6-promise

--- src/main.js
+++ src/main.js
@@ -9,18 +9,19 @@ import http from '@/util/http'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import $ from 'jquery'
-import promise from 'es6-promise';
-promise.polyfill();
-
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI)
 Vue.use(http)
 
+// 设置options 不进行发送option 验证  ------错误用法？
+// Vue.http.options.emulateJSON = true
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // store,
   template: '<App/>',
   components: { App }
 })
@@ -30,8 +31,6 @@ new Vue({
 Vue.filter('dateFilter', function(value) {
   if (!value) return ''
   console.log(value);
+  // console.log(new Date(value).toLocaleString().replace(/\//g,'-').match(/\d{4}\-\d{2}\-\d{1,2}/)[0])
   return new Date(value).getFullYear() + '-' + (new Date(value).getMonth() + 1) + '-' + new Date(value).getDate();
 })
-
-
-
