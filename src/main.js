3dd83562be8ccbb903ad0afb4a3c1a6bb7f328e1
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 19:19:59 +0800

    解决报错

--- src/main.js
+++ src/main.js
@@ -4,21 +4,28 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import "babel-polyfill"
+// import store from './store/store'
 import qs from 'qs'
 import http from '@/util/http' 
-import validation from '@/util/validation' 
+// import validation from '@/util/validation' 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import $ from 'jquery'
+// import promise from 'es6-promise';
+// promise.polyfill();
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI)
 Vue.use(http)
 
+// 设置options 不进行发送option 验证  ------错误用法？
+// Vue.http.options.emulateJSON = true
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  // store,
   template: '<App/>',
   components: { App }
 })
