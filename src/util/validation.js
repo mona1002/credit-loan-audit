e1123c6c47fc6cb0821976622e8e9e9217442dda
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 19:27:15 +0800

    测试文件

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,23 +1,2 @@
 console.log("aaaaa")
 
-// import Vue from 'vue';
-// import VeeValidate,{Validator} from 'vee-validate';
-// import zh from 'vee-validate/dist/locale/zh_CN'
-// import messages from '../assets/js/zh_CN';
-
-
-// Validator.updateDictionary({
-//     zh_CN: {
-//         messages
-//     }
-// });
-
-// const config = {
-//   locale: 'zh_CN', 
-//   strict: true,
-//   // events: 'input|blur',
-//   delay: 1000, 
-
-// };
-
-// Vue.use(VeeValidate, config);
