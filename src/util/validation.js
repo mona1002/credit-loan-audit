f4c0dfbd15d5fb1dd94b4f7cae68e50e8e39dd6c
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 20:11:25 +0800

    更改ui样式

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,22 +1,14 @@
 import Vue from 'vue';
 import VeeValidate,{Validator} from 'vee-validate';
-import zh from 'vee-validate/riskWeb/locale/zh_CN'
 import messages from '../assets/js/zh_CN';
-
-
 Validator.updateDictionary({
     zh_CN: {
         messages
     }
 });
-
-
 const config = {
   locale: 'zh_CN', 
   strict: true,
-  // events: 'input|blur',
   delay: 1000, 
-
 };
-
 Vue.use(VeeValidate, config);
