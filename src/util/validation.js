d73d69d3ab519a6a0b859a4c0e9b445500611ee7
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 19:53:46 +0800

    测试文件更改路径

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,5 +1,7 @@
 console.log("aaaaa")
 
+import Vue from 'vue';
+import VeeValidate,{Validator} from 'vee-validate';
 import zh from 'vee-validate/dist/locale/zh_CN'
 import messages from '../assets/js/zh_CN';
 
