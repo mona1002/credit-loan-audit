7578b204f17d8b93d352d8b28645eb2578b3678c
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 20:15:30 +0800

    测试

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,14 +1,7 @@
 import Vue from 'vue';
 import VeeValidate,{Validator} from 'vee-validate';
+import zh from 'vee-validate/dist/locale/zh_CN'
 import messages from '../assets/js/zh_CN';
-Validator.updateDictionary({
-    zh_CN: {
-        messages
-    }
-});
-const config = {
-  locale: 'zh_CN', 
-  strict: true,
-  delay: 1000, 
-};
+console.log("aaaaa")
+
 Vue.use(VeeValidate, config);
