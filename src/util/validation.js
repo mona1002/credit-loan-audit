5da0e683a1b4d4f942661d4028f05c00a009414f
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2017-12-20 19:17:43 +0800

    测试文件

--- src/util/validation.js
+++ src/util/validation.js
@@ -1,22 +1,23 @@
-import Vue from 'vue';
-import VeeValidate,{Validator} from 'vee-validate';
-import zh from 'vee-validate/dist/locale/zh_CN'
-import messages from '../assets/js/zh_CN';
-
-
-Validator.updateDictionary({
-    zh_CN: {
-        messages
-    }
-});
-
-const config = {
-  locale: 'zh_CN', 
-  strict: true,
-  // events: 'input|blur',
-  delay: 1000, 
-
-};
-
 console.log("aaaaa")
-Vue.use(VeeValidate, config);
+
+// import Vue from 'vue';
+// import VeeValidate,{Validator} from 'vee-validate';
+// import zh from 'vee-validate/dist/locale/zh_CN'
+// import messages from '../assets/js/zh_CN';
+
+
+// Validator.updateDictionary({
+//     zh_CN: {
+//         messages
+//     }
+// });
+
+// const config = {
+//   locale: 'zh_CN', 
+//   strict: true,
+//   // events: 'input|blur',
+//   delay: 1000, 
+
+// };
+
+// Vue.use(VeeValidate, config);
