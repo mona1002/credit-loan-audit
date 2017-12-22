1ba28810208b95f00eee243aa5b6d644356d6a9d
liwangjun <459378261@qq.com>
2017-12-22 10:40:58 +0800

    工作台  统一登录平台    getUserInfo   ( 统一登录平台 过来 需要 请求 用户 信息 ) , 正常登录   逻辑

--- src/view/FirstTrail/workbench.vue
+++ src/view/FirstTrail/workbench.vue
@@ -70,209 +70,227 @@
   </div>
 </template>
 <script>
-  import myHead from "../header.vue"
-  export default {
-    data() {
-      return {
-        userInf: '',
-        activeNames: ['1'],
-        activeNames1: ['1'],
-        activeNames2: ['1'],
-        taskStatus: '', //任务状态
-        loginPass: [],
-        userCode: '', //用户编码
-        orgCode: '', //机构编码
-        pageNum: '', //页数（第几页）
-        pageSize: '', //页面显示行数
-        processTemplateId: '', // 流程模板Id
-        taskNodeName: '', // 任务节点名称
-        tableData: [],
-        currentRow: null,
-        workbenchPass: {
-          processTemplateId: '',
-          taskStatus: "01",
-          taskNodeName: ''
-        }
+import myHead from "../header.vue"
+export default {
+  data() {
+    return {
+      userInf: '',
+      activeNames: ['1'],
+      activeNames1: ['1'],
+      activeNames2: ['1'],
+      taskStatus: '', //任务状态
+      loginPass: [],
+      userCode: '', //用户编码
+      orgCode: '', //机构编码
+      pageNum: '', //页数（第几页）
+      pageSize: '', //页面显示行数
+      processTemplateId: '', // 流程模板Id
+      taskNodeName: '', // 任务节点名称
+      tableData: [],
+      currentRow: null,
+      workbenchPass: {
+        processTemplateId: '',
+        taskStatus: "01",
+        taskNodeName: ''
       }
+    }
+  },
+  components: {
+    myHead
+  },
+  methods: {
+    more() {
+      console.log("more")
+      this.$store.commit('workB', {
+        processTemplateId: "56789",
+        taskNodeName: "67uj"
+      })
+      console.log(this.$store.state.First)
     },
-    components: {
-      myHead
-    },
-    methods: {
-      more() {
-        console.log("more")
-        this.$store.commit('workB', {
-          processTemplateId: "56789",
-          taskNodeName: "67uj"
-        })
-        console.log(this.$store.state.First)
-      },
-      handleCurrentChange(val) {
-        console.log("我是表格")
-        console.log(val.taskNodeName)
-        if (val.taskNodeName == "creditApp_firstTrial") {
-          this.currentRow = val;
-          this.workbenchPass.processTemplateId = val.processTemplateId;
-          this.workbenchPass.taskNodeName = val.taskNodeName;
-          // this.$store.state
-          // console.log(this.$store.state.First.Workbench="ghjkld67890")
-          // this.$store.state.First.Workbench=this.workbenchPass;
-          // console.log(this.$store.state.First)
+    handleCurrentChange(val) {
+      console.log("我是表格")
+      console.log(val.taskNodeName)
+      if (val.taskNodeName == "creditApp_firstTrial") {
+        this.currentRow = val;
+        this.workbenchPass.processTemplateId = val.processTemplateId;
+        this.workbenchPass.taskNodeName = val.taskNodeName;
+        // this.$store.state
+        // console.log(this.$store.state.First.Workbench="ghjkld67890")
+        // this.$store.state.First.Workbench=this.workbenchPass;
+        // console.log(this.$store.state.First)
 
-          localStorage.setItem("workbenchPass", JSON.stringify(this.workbenchPass)); //工作台部分信息，带入workbenchPass
-          this.$router.push({
-            path: '/taskInWaitting',
-            // query: {
-            //   processTemplateId: val.processTemplateId,
-            //   taskNodeName: val.taskNodeName,
-            //   taskStatus: "01",
-            //   userCode: this.loginPass.userCode,
-            //   orgCode: this.loginPass.orgCode
-            // }
-          });
-          // localStorage.setItem("tableData", JSON.stringify(tableData));
-          // this.$router.push({path:'/taskInWaitting',query:'123'})
-          //  console.log(111, this.$route.query.picName)   接参数
-        } else if (val.taskNodeName == "creditApp_finalTrial_one" || val.taskNodeName == "creditApp_finalTrial_two" ||
-          val.taskNodeName == "creditApp_finalTrial_three" || val.taskNodeName == "creditApp_finalTrial_four" || val.taskNodeName ==
-          "creditApp_finalTrial_five") {
-          this.currentRow = val;
-          this.workbenchPass.processTemplateId = val.processTemplateId;
-          this.workbenchPass.taskNodeName = val.taskNodeName;
-          localStorage.setItem("FinalWorkbenchPass", JSON.stringify(this.workbenchPass)); //工作台部分信息，带入workbenchPass
-          // this.$router.push({
-          //   path: '/taskInWaitting',
-          // });
-        }
-
-
-      }
-
-    },
-    computed: {
-
-    },
-    mounted() {
-      // 字段
-      // ASSIGNED("01", "代办"),
-      // COMPLETED("03","已办"),
-      // ABORTED("04","历史"),
-      //  获取到 路由传参 
-      // this.userCode = this.$route.query.userCode;
-      // this.orgCode = this.$route.query.orgCode;
-      // console.log(this.userCode + "================" + this.orgCode)
-      // this.$route.query.picName接参数 
-
-      //  this.get("/smUser/getUserInfo").then(response => {
-
-      this.get("http://testplatform.nuoyuan.com.cn:20717/remote/user/getUserInfo").then(response => {
-        // axios 请求
-        // console.info(response.data);
-        // this.userInf=response.data;
-        this.userInf = {
-          userCode: response.data.userCode,
-          orgCode: response.data.orgCode,
-        }
-        // console.log(this.userInf.orgCode)
-        // console.log(this.userInf.userCode)
-        localStorage.setItem("userInf", JSON.stringify(this.userInf));
-        this.post("/workFlowTaskQuery/getTaskProfile", {
-          taskStatus: "01",
-          // userCode: this.userInf.userCode,
-          // orgCode: this.userInf.orgCode
-        }).then(res => {
-          console.log(res.data);
-          this.tableData = res.data;
+        localStorage.setItem("workbenchPass", JSON.stringify(this.workbenchPass)); //工作台部分信息，带入workbenchPass
+        this.$router.push({
+          path: '/taskInWaitting',
+          // query: {
+          //   processTemplateId: val.processTemplateId,
+          //   taskNodeName: val.taskNodeName,
+          //   taskStatus: "01",
+          //   userCode: this.loginPass.userCode,
+          //   orgCode: this.loginPass.orgCode
+          // }
         });
-      });
+        // localStorage.setItem("tableData", JSON.stringify(tableData));
+        // this.$router.push({path:'/taskInWaitting',query:'123'})
+        //  console.log(111, this.$route.query.picName)   接参数
+      } else if (val.taskNodeName == "creditApp_finalTrial_one" || val.taskNodeName == "creditApp_finalTrial_two" ||
+        val.taskNodeName == "creditApp_finalTrial_three" || val.taskNodeName == "creditApp_finalTrial_four" || val.taskNodeName ==
+        "creditApp_finalTrial_five") {
+        this.currentRow = val;
+        this.workbenchPass.processTemplateId = val.processTemplateId;
+        this.workbenchPass.taskNodeName = val.taskNodeName;
+        localStorage.setItem("FinalWorkbenchPass", JSON.stringify(this.workbenchPass)); //工作台部分信息，带入workbenchPass
+        // this.$router.push({
+        //   path: '/taskInWaitting',
+        // });
+      }
 
-      // this.loginPass = JSON.parse(localStorage.getItem('userInf'));
-      // this.post("/workFlowTaskQuery/getTaskProfile", {
-      //   taskStatus: "01",
-      //   // userCode: "ddyy",
-      //   // orgCode: "021"
-      // }).then(res => {
-      //   console.log(res.data);
-      //   this.tableData = res.data;
-      // });
-    },
-  }
+
+    }
+
+  },
+  computed: {
+
+  },
+  mounted() {
+    // 字段
+    // ASSIGNED("01", "代办"),
+    // COMPLETED("03","已办"),
+    // ABORTED("04","历史"),
+    //  获取到 路由传参 
+    // this.userCode = this.$route.query.userCode;
+    // this.orgCode = this.$route.query.orgCode;
+    // console.log(this.userCode + "================" + this.orgCode)
+    // this.$route.query.picName接参数 
+
+    //  this.get("/smUser/getUserInfo").then(response => {
+
+// 统一登录平台  调试   start 
+    // this.get("http://testplatform.nuoyuan.com.cn:20717/remote/user/getUserInfo").then(response => {
+    // axios 请求
+    // console.info(response.data);
+    // this.userInf=response.data;
+    // this.userInf = {
+    //   userCode: response.data.userCode,
+    //   orgCode: response.data.orgCode,
+    // }
+    // console.log(this.userInf.orgCode)
+    // console.log(this.userInf.userCode)
+// 统一登录 平台  调测 end
+
+
+    //取 登录信息  勿动!!!  start
+    localStorage.setItem("userInf", JSON.stringify(this.userInf));
+    this.post("/workFlowTaskQuery/getTaskProfile", {
+      taskStatus: "01",
+      // userCode: this.userInf.userCode,
+      // orgCode: this.userInf.orgCode
+    }).then(res => {
+      console.log(res.data);
+      this.tableData = res.data;
+    });
+    //取 登录信息  勿动!!!  end
+
+// });
+// 统一登录 平台  闭合标签 end
+
+    // this.loginPass = JSON.parse(localStorage.getItem('userInf'));
+    // this.post("/workFlowTaskQuery/getTaskProfile", {
+    //   taskStatus: "01",
+    //   // userCode: "ddyy",
+    //   // orgCode: "021"
+    // }).then(res => {
+    //   console.log(res.data);
+    //   this.tableData = res.data;
+    // });
+  },
+}
 
 </script>
 <style scoped>
-  /* public 部分 */
+/* public 部分 */
 
-  .moreC {
-    float: right;
-    margin-right: 35px;
-  }
+.moreC {
+  float: right;
+  margin-right: 35px;
+}
 
-  .border_top_bottom {
-    border-top: 1px solid gray;
-  }
-  /* 大框  */
+.border_top_bottom {
+  border-top: 1px solid gray;
+}
 
-  .workbench {
-    background: #ededed;
-    width: 100%;
-    height: 100%;
-  }
 
-  .workbench .top {
-    height: 70px;
-  }
+/* 大框  */
 
-  .workbench .main {
-    height: calc( 100% - 70px);
-    overflow: hidden;
-    background: #ededed;
-  }
-  /* main */
+.workbench {
+  background: #ededed;
+  width: 100%;
+  height: 100%;
+}
 
-  .main .main_left,
-  .main .main_right {
-    float: left;
-  }
-  /* 左边-常用 */
+.workbench .top {
+  height: 70px;
+}
 
-  .main .main_left {
-    height: 100%;
-    width: 148px;
-    background: white;
-    text-align: center;
-  }
+.workbench .main {
+  height: calc( 100% - 70px);
+  overflow: hidden;
+  background: #ededed;
+}
 
-  .main .main_left h2 {
-    font-size: 16.5px;
-    margin-top: 3px;
-  }
-  /* 右边-折叠面板 */
 
-  .main .main_right {
-    width: calc( 100% - 148px);
-    background: white;
-  }
-  /* 代办任务 */
+/* main */
 
-  .main_right .main_right_task,
-  .main_right .main_right_work {
-    float: left;
-    width: 50%;
-    height: 100vh;
-    padding: 19px 20px 0 20px;
-    /* background: black; */
-    background: #ededed;
-  }
+.main .main_left,
+.main .main_right {
+  float: left;
+}
 
-  .main_right .main_right_work {
-    padding-left: 0;
-  }
 
-  .waitting {
-    /* height:800px; */
-    background: red;
-  }
+/* 左边-常用 */
 
-  @media screen and (min-width: 1366px) {}
+.main .main_left {
+  height: 100%;
+  width: 148px;
+  background: white;
+  text-align: center;
+}
+
+.main .main_left h2 {
+  font-size: 16.5px;
+  margin-top: 3px;
+}
+
+
+/* 右边-折叠面板 */
+
+.main .main_right {
+  width: calc( 100% - 148px);
+  background: white;
+}
+
+
+/* 代办任务 */
+
+.main_right .main_right_task,
+.main_right .main_right_work {
+  float: left;
+  width: 50%;
+  height: 100vh;
+  padding: 19px 20px 0 20px;
+  /* background: black; */
+  background: #ededed;
+}
+
+.main_right .main_right_work {
+  padding-left: 0;
+}
+
+.waitting {
+  /* height:800px; */
+  background: red;
+}
+
+@media screen and (min-width: 1366px) {}
 
 </style>
