a5efdfc3bb2f625bc27988d93cc7138cd5b3b487
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-03-01 11:23:24 +0800

    反欺诈审批结论轨迹，风险项改为riskSection取值，添加大数据，公积金等主页面+风控子页面

---
+++ src/view/SocialSeAndPneCtrl/PneCtrl/BaiRongData.vue
@@ -0,0 +1,214 @@
+<template>
+  <div class="SSComp">
+    <!-- 大数据风控 -->
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="1">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">输入参数</span>
+        </template>
+        <div class="lists">
+          <ul>
+            <li>
+              <p>
+                <label>身份证：</label>
+              </p>
+              <p>
+                <label>手机号：</label>
+              </p>
+              <p>
+                <label>姓名：</label>
+              </p>
+            </li><li>
+            <p>
+              <label>联系人手机号：</label>
+            </p>
+           </li>
+          </ul>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="2">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">风险详情</span>
+        </template>
+        <div class="lists">
+          <ul>
+            <li>
+              <p>
+                <label>策略集：</label>
+              </p>
+              <p>
+                <label>风险状态：</label>
+              </p>
+              <p>
+                <label>风险系数：</label>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="3">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">命中规则</span>
+        </template>
+        <div class="tes">
+          <div class="tab_wrap">
+            <el-table :data="tableData" border height="100%" style="width: 100%">
+              <el-table-column prop="date" label="规则编号" align="center" min-width="70">
+              </el-table-column>
+              <el-table-column prop="name" label="规则名称" align="center" min-width="70">
+              </el-table-column>
+              <el-table-column prop="address" label="规则分数" align="center" min-width="100">
+              </el-table-column>
+              <el-table-column prop="address" label="规则结果" align="center" min-width="100">
+              </el-table-column>
+              <el-table-column prop="address" label="查看规则详情" align="center" min-width="100">
+              </el-table-column>
+            </el-table>
+          </div>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeNames: ['1', "2", "3"],
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
+      }
+    },
+    mounted(){
+            this.get("/credit/queryCityCounty", {
+          parentCode: this.checkData.workProvince,
+        }).then(res => {
+          if (res.statusCode == 200) {
+           
+          } else {
+            this.$message.error('查询失败');
+          }
+        })
+    }
+  }
+
+</script>
+<style scoped>
+  .SSComp {
+    overflow-x: hidden;
+    height: 100%;
+  }
+
+  .icon_hat {
+    position: absolute;
+    top: 12px;
+    left: 14px;
+  }
+
+  .lists {
+    padding: 10px 0 0 21px;
+  }
+
+  .SSComp p {
+    float: left;
+    width: 33.3%;
+    margin-bottom: 10px;
+  }
+
+  .SSComp label {
+    display: inline-block;
+    width: 95px;
+    text-align: right;
+  }
+
+  .tab_wrap {
+  margin: 10px;
+  border-radius: 4px;
+    
+    overflow-x: hidden;
+    /* border: 1px solid red; */
+  }
+.tes{
+  /* max-height: 500px; */
+
+  /* border: 3px solid green; */
+}
+</style>
---
+++ src/view/SocialSeAndPneCtrl/SocialSeComponents/AccumulationFund.vue
@@ -0,0 +1,145 @@
+<template>
+  <div class="SSComp">
+    <!-- 公积金 -->
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="1">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">输入参数</span>
+        </template>
+        <div class="lists">
+          <ul>
+            <li>
+              <p>
+                <label>姓名：</label>
+              </p>
+              <p>
+                <label>身份证号：</label>
+              </p>
+              <p>
+                <label>账号：</label>
+              </p>
+            </li>
+            <li>
+              <p>
+                <label>密码：</label>
+              </p>
+              <p>
+                <label>省市：</label>
+              </p>
+            </li>
+          </ul>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="2">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">基本信息</span>
+        </template>
+        <div class="lists">
+          <h4>基本信息</h4>
+
+          <h4>账单记录</h4>
+          <h4>贷款记录</h4>
+          <h4>还款记录</h4>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+    <el-collapse v-model="activeNames">
+      <el-collapse-item name="3">
+        <template slot="title">
+          <img src="../../../../static/images/C4A8A526-401A-43D1-B835-5EFEBC7E2F23@1x.png" class="icon_hat">
+          <span class="headFont">报告信息</span>
+        </template>
+        <div class="lists">
+          <h4>用户及账户基本信息</h4>
+          <h4>用户基本信息效验</h4>
+          <h4>账户基本信息</h4>
+          <h4>缴纳信息</h4>
+          <h4>还款信息</h4>
+        </div>
+      </el-collapse-item>
+    </el-collapse>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeNames: ['1', "2", "3"],
+        tes:[]
+      }
+    },
+    mounted(){
+        //          this.get("../../../../static/json/a.json", {
+        // //   parentCode: this.checkData.workProvince,
+        // }).then(res => {
+        //   if (res.statusCode == 200) {
+        //    console.log(res)
+        //   } else {
+        //     this.$message.error('查询失败');
+        //   }
+        // })
+
+
+// this.post("/creAccepLoanDetailInfo/getAccepLoanDetailInfo", {
+// 		        'id':"78351fc3-5ca6-4d37-ac48-e7a69a8abc70"
+// 		      }).then(res => {
+// console.log(res.data)
+// for(var i in res.data){
+//     var y=[];
+// console.log(i +':'+ res.data[i])
+// y[0]=i;
+// y[1]=res.data[i];
+// this.tes.push(y);
+// }
+// console.log(this.tes)
+
+//               })
+var obj={
+    name:2,
+    name:4,
+    age:3
+}
+    for(var i in obj){
+console.log(i +':'+ obj[i])
+
+    }
+
+	
+  }
+  }
+</script>
+<style scoped>
+  .SSComp {
+    overflow-x: hidden;
+    height: 100%;
+  }
+
+  .icon_hat {
+    position: absolute;
+    top: 12px;
+    left: 14px;
+  }
+
+  .lists {
+    padding: 10px 0 0 21px;
+  }
+
+  .SSComp p {
+    float: left;
+    width: 33.3%;
+    margin-bottom: 10px;
+  }
+  .SSComp h4{
+clear: both;
+  }
+  .SSComp label {
+    display: inline-block;
+    width: 65px;
+    text-align: right;
+  }
+
+</style>
