5f00767a9e20eb20c0060c96c550c40fdef983cc
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-03-12 10:59:41 +0800

    导航重新布局，最外层页面添加最小宽度和x隐藏

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,90 @@
 <template>
-  <div id="app">
+<div id="app">
+  <!-- <div id="aaa"> -->
+    <router-view class="Abody main" @onbeforeunload="aaa "></router-view>
+    <!-- <tag class="fottt"></tag> -->
+  <!-- </div> -->
+</div>
 
-       <!-- <myHead></myHead> -->
-       <!-- <img src="../static/img/20171122102421.png" width="100%"> -->
-<!-- <el-row class="header"></el-row> -->
-    <router-view class="Abody"></router-view>
-    <div v-show="false" > </div>
-  </div>
 
 </template>
 
 <script>
-// import myHead from "./view/header"
+  // import myHead from "./view/header"
+  // import tag from './view/tag.vue'
+  // import appConstant from './util/constant'
+
   export default {
-data(){
-return{
-  // 标识：01 初审-工作台 ；  02  终审-工作台； 03 初审-内部匹配 ； 04 终审-内部匹配；
-  // localstorage字段：  judge：{flag：01 }  （示例）
-}
-},
-    mounted(){
-      // console.log(this.$refs.ddd)
-    
-// taskNodeName:
-
-// 初审审批
-// creditApp_firstTrial   
-
-// 终审审批
-// creditApp_finalTrial_one
-// creditApp_finalTrial_two
-// creditApp_finalTrial_three
-// creditApp_finalTrial_four
-// creditApp_finalTrial_five
-
+    data() {
+      return {
+        // 标识：01 初审-工作台 ；  02  终审-工作台； 03 初审-内部匹配 ； 04 终审-内部匹配；
+        // localstorage字段：  judge：{flag：01 }  （示例）
+      }
     },
-updated () {
-  // this.$nextTick(function () {
-  //   // Code that will run only after the
-  //   // entire view has been re-rendered
-  //     var k=document.getElementsByClassName("el-select-dropdown el-popper")
-  //     console.log(document.getElementsByClassName("el-select-dropdown el-popper") )
-  //     console.log(k)
-  // })
-},
-  
-// components:{
-//   myHead
-// }
-}
+    methods: {
+      aaa() {
+        console.log("刷新");
+        // this.$created(function(){
+        //   console.log("页面刷新啦啦啦啦")
+        // })
+
+        // 方法1
+        //  this.$router.push({
+        //         path: '/',
+        //       });
+        // 方法2
+        // window.location.href = appConstant.path;
+      },
+      reload() {
+        // location.reload()
+      }
+    },
+    created() {
+      // 存在 localStorage 的缓存内容
+      // console.log("页面刷新")
+      // if (localStorage.data) {
+      //   this.myData = JSON.parse(localStorage.data)
+      // }
+      // else {
+      //   // 页面无缓存内容时，初始化数据并写入缓存
+      //   this.initData()
+      // }
+    },
+    mounted() {
+      // this.$router.push({
+      //   path: '/',
+      // });
+      // 初审审批
+      // creditApp_firstTrial    
+
+      // 终审审批
+      // creditApp_finalTrial_one
+      // creditApp_finalTrial_two
+      // creditApp_finalTrial_three
+      // creditApp_finalTrial_four
+      // creditApp_finalTrial_five
+    },
+    components: {
+      // tag,
+    }
+  }
 
 </script>
 
 <style>
   @import "../static/css/common.css";
   @import "../static/css/element_UI.css";
-  /* .header{
-    width: 100%;
-    height: 60px;
-    line-height: 60px;
-    background: #20a0ff;
+  /* .main{
+  height: 800px;
+  overflow: hidden;
+} */
+
+  /* .fottt {
+    position: absolute;
+    bottom: 0;
   } */
+#app{
+  min-width: 1366px;
+  overflow-x: hidden;
+}
 </style>
