ad763fe4c52d7b9af3fb6636ff9bf6af8191d72c
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-03-30 11:23:03 +0800

    工作台去掉工作通知和公司图动态占位字

--- src/view/FirstTrail/workbench.vue
+++ src/view/FirstTrail/workbench.vue
@@ -46,8 +46,8 @@
                     <img src="../../../static/images/shuaxin.png" class="moreCIcon">
                   </span>
                 </template>
-                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                <div>  </div>
+                <div>   </div>
               </el-collapse-item>
             </el-collapse>
           </div>
@@ -62,8 +62,8 @@
                     <img src="../../../static/images/shuaxin.png" class="moreCIcon">
                   </span>
                 </template>
-                <div>与现实生活一致：与现实生活的流程、逻辑保持一致，遵循用户习惯的语言和概念；</div>
-                <div>在界面中一致：所有的元素和结构需保持一致，比如：设计样式、图标和文本、元素的位置等。</div>
+                <div>    </div>
+                <div>   </div>
               </el-collapse-item>
             </el-collapse>
           </div>
