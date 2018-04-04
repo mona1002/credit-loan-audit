e0073d072502612672798f3c0fd1f6da3dd6a4d3
yueyanyan <yueyanyan@nuoyuan.com.cn>
2018-04-04 15:14:16 +0800

    再次修改未定义问题

--- src/util/constant.js
+++ src/util/constant.js
@@ -9,7 +9,7 @@ var host = location.hostname,
   developmentEnv = "testriskmgt.nuoyuan.com.cn", // 开发环境
   testEnv = "testriskmgt.nuoyuan.com.cn", // 测试环境
   releaseEnv = "preriskmgt.nuoyuan.com.cn", // 准生产环境
-  productionEnv = "riskmgt.nuoyuan.com.cn"; // 生产环境
+  productionEnv = "riskmgt.nuoyuan.com.cn", // 生产环境
   press = "ycriskmgt.nuoyuan.com.cn"; // 压测环境
 if (host === productionEnv) {
   host = productionEnv;
