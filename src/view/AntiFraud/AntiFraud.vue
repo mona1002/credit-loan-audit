317f782a6b55ee17233032a3f55084fc2d988c43
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-04-11 10:40:45 +0800

    反欺诈列表去掉右侧固定列

--- src/view/AntiFraud/AntiFraud.vue
+++ src/view/AntiFraud/AntiFraud.vue
@@ -80,7 +80,7 @@
           </el-table-column>
           <el-table-column prop="fraudStateTxt" label="反欺诈状态" width="120">
           </el-table-column>
-          <el-table-column label="操作" width="180" fixed="right">
+          <el-table-column label="操作" width="180">
             <!-- <template slot-scope="scope">
             <el-button size="mini" @click="handleClickInfo(scope.row)">查看</el-button>
             <el-button size="mini" @click="handleClickEdit(scope.row)">编辑</el-button>
