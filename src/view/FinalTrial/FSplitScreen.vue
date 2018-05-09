2c15eb1606d224224bfc2ff0a72fd479d6fe6233
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-05-09 13:47:36 +0800

    ie

--- src/view/FinalTrial/FSplitScreen.vue
+++ src/view/FinalTrial/FSplitScreen.vue
@@ -287,14 +287,16 @@
             if (dragging) {
               clickX = e.pageX;
               if (clickX > leftOffset + 10 && clickX < (wrapWidth - 5)) {
+                console.log( ' y')
                 nextW2 = clickX - leftOffset;
-                labBtn.eq(0).css('left', clickX - leftOffset + 2 + 'px'); //按钮移动
-                labBtn.eq(0).prev().width(clickX - leftOffset + 'px'); //前一个div宽度变化
-                labBtn.eq(0).next().width(wrapWidth - nextW2 - 6 + 'px'); //减多少宽地待算
+                labBtn.eq(0).css('left', clickX - leftOffset + 2 + 'px');
+                labBtn.eq(0).prev().width(clickX - leftOffset + 'px');
+                labBtn.eq(0).next().width(wrapWidth - nextW2 - 6 + 'px'); 
               } else if (clickX < leftOffset + 10 && clickX < (wrapWidth - 5)) {
+                console.log( ' z')
                 labBtn.eq(0).css('left', '0px');
                 labBtn.eq(0).prev().width('0px');
-                labBtn.eq(0).next().width(wrapWidth - 6 + 'px'); //减多少宽地待算
+                labBtn.eq(0).next().width(wrapWidth - 6 + 'px'); 
               }
               if (clickX > (wrapWidth - 5)) {
                 labBtn.eq(0).css('left', parseFloat(wrapWidth) - 11 + 'px');
