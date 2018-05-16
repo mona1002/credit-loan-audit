a8db45838985c5ef71bcfa0920488945f4f2fae1
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-05-16 15:47:09 +0800

    添加开发环境

--- src/util/constantUser.js
+++ src/util/constantUser.js
@@ -5,7 +5,7 @@ var appConstantUser = {};
 // url 配置
 var host = location.hostname,
   port = 80,
-  developmentEnv = "testriskmgt.nuoyuan.com.cn", // 开发环境
+  developmentEnv = "coderiskmgt.nuoyuan.com.cn", // 开发环境
   testEnv = "testriskmgt.nuoyuan.com.cn", // 测试环境
   releaseEnv = "preriskmgt.nuoyuan.com.cn", // 准生产环境
   productionEnv = "riskmgt.nuoyuan.com.cn", // 生产环境
@@ -20,8 +20,7 @@ if (host === productionEnv) {
 }else if (host === press) {
   host = "http://ycplatform.nuoyuan.com.cn/";
 } else {
-  host = "http://testplatform.nuoyuan.com.cn/";
-
+  host = "http://codeplatform.nuoyuan.com.cn/";
 }
 
 appConstantUser.path=host;
