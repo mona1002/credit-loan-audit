e2cf222180581e570558ea6b5a9ff684e70634ac
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-05-16 16:02:32 +0800

    测试开发

--- src/view/header.vue
+++ src/view/header.vue
@@ -100,7 +100,7 @@
         },
         backLoin() {
           console.log(UserURL)
-          window.location.href = UserURL + "#/workbench";
+          // window.location.href = UserURL + "#/workbench";
           localStorage.clear();
         },
         le(){
