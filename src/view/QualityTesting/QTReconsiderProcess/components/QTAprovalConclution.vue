8d00d17a18948e96a43989214ebc885d91d9db8a
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-05-28 11:43:47 +0800

    解决测试问题

---
+++ src/view/QualityTesting/QTReconsiderProcess/components/QTAprovalConclution.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="aAntiConclusionPath">
+    <!-- 反欺诈审批结论轨迹  质检页面-->
+    <el-table :data="tableData" style="width: 100%" border>
+      <el-table-column prop="auditResultTxt" label="审批结果" min-width="35">
+      </el-table-column>
+      <el-table-column prop="mainreaName" label="主原因" min-width="35">
+      </el-table-column>
+      <el-table-column prop="subreaName" label="子原因" min-width="35">
+      </el-table-column>
+      <el-table-column prop="riskSection" label="风险项" min-width="35">
+      </el-table-column>
+      <el-table-column prop="auditDesc" label="审批描述" >
+      </el-table-column>
+      <el-table-column prop="auditTime" label="审批时间" min-width="35" align="center">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        tastwaitingPass: '',
+        tableData: []
+      }
+    },
+    mounted() {
+      // this.judgeFlag = JSON.parse(localStorage.getItem("judge"));
+      // if (this.judgeFlag.flag == '03') {
+      //   this.tastwaitingPass = JSON.parse(localStorage.getItem("AntitaskInWaitting")); //反欺诈专员-匹配查看
+      // } else if (this.judgeFlag.flag == '04') {
+      //   this.tastwaitingPass = JSON.parse(localStorage.getItem("AntiManagertaskInWaitting")); //反欺诈主管-匹配查看
+      // }
+       this.tastwaitingPass = JSON.parse(localStorage.getItem("QT")) //质检
+      this.post("/fraudAuditOpinion/getApproveConclusionTrack", {
+        appinfoId: this.tastwaitingPass.businessId,
+      }).then(res => {
+        if (res.statusCode == 200) {
+          this.tableData = res.data;
+        } else {
+          this.$message.error(res.msg);
+        }
+      });
+    }
+  }
+
+</script>
+<style scoped>
+
+
+</style>
