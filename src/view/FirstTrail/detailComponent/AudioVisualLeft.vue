deecff8b737c2c888b360e11f14c8fab9eed6048
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-08-01 18:05:10 +0800

    合并准生产

--- src/view/FirstTrail/detailComponent/AudioVisualLeft.vue
+++ src/view/FirstTrail/detailComponent/AudioVisualLeft.vue
@@ -78,8 +78,11 @@
         <figure v-for="(val,index) in pngAyyrs" :key="index" class="small_pic_figure" v-show="SmallmyPic">
           <img class="Small_pic" :src="imgBaseUrl+val.imagePath" @click="ChangeCss(index)" @dblclick="smallPic($event,index)" ref="small_pic_ref"
           />
-          <em :style="'background:url('+imgBaseUrl+val.imagePath+')'" class="audio_img"></em>
-          <em style="background:url(http://10.1.26.6:8080/ptopCredit/download/downloadAction!download.action?filepath=/creditFile&filename=upload\2015-11-19\201511190173044032\B1-1.png)" class="audio_img"></em>
+          <!-- <em :style="'background:url('+imgBaseUrl+val.imagePath+') '" class="audio_img"></em> -->
+
+
+
+          <!-- <em style="background:url(http://10.1.26.6:8080/ptopCredit/download/downloadAction!download.action?filepath=/creditFile&filename=upload\2015-11-19\201511190173044032\B1-1.png)" class="audio_img"></em> -->
           
           <p v-if="SmallmyPic"> {{val.arcSubType}} </p>
         </figure>
