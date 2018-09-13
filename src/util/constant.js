603d0245b9e25b9c1ad643f9c0f5a761bc8bcca1
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-09-13 15:51:59 +0800

    分支

--- src/util/constant.js
+++ src/util/constant.js
@@ -22,7 +22,7 @@ if (host === productionEnv) {
 } else if (host === developmentEnv) {
   host = developmentEnv;
 } else {
-  host = releaseEnv;
+  host = developmentEnv;
 }
 
 appConstant.baseUrl_common = "http://" + host + (80 === port ? '' : (':' + port)) + "/riskManagement";
