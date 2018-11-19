63fe77abc4fd6e5bd16156aa761c02e64144bc97
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-11-19 14:42:45 +0800

    复议授权期限下拉借口

--- src/view/Reconsider/ReconConclusion.vue
+++ src/view/Reconsider/ReconConclusion.vue
@@ -280,8 +280,8 @@
                   </el-form-item>
                   <el-form-item class="fl alert_collapse_inputLabel" label="授信期限[月]：" :label-width="formApproLab">
                     <el-select v-model="creditExtensionLoanTerm ">
-                      <el-option v-for="item in creditExtensionLoanTermTerms " :label="item.appDuration " :value="item "
-                        :key="item.appDuration ">
+                      <el-option v-for="item in creditExtensionLoanTermTerms " :label="item.code " :value="item.code"
+                        :key="item.code ">
                       </el-option>
                     </el-select>
                   </el-form-item>
@@ -588,33 +588,6 @@
           }
         })
       },
-      //保留两位小数 整数千分位
-      // formatNumber(num, cent, isThousand) {
-      //   num = num.toString().replace(/\$|\,/g, '');
-      //   // 检查传入数值为数值类型
-      //   if (isNaN(num))
-      //     num = "0";
-      //   // 获取符号(正/负数)
-      //   let sign = (num == (num = Math.abs(num)));
-      //   num = Math.floor(num * Math.pow(10, cent) + 0.50000000001); // 把指定的小数位先转换成整数.多余的小数位四舍五入
-      //   let cents = num % Math.pow(10, cent); // 求出小数位数值
-      //   num = Math.floor(num / Math.pow(10, cent)).toString(); // 求出整数位数值
-      //   cents = cents.toString(); // 把小数位转换成字符串,以便求小数位长度
-      //   // 补足小数位到指定的位数
-      //   while (cents.length < cent)
-      //     cents = "0" + cents;
-      //   for (var i = 0; i < Math.floor((num.length - (1 + i)) / 3); i++)
-      //     num = num.substring(0, num.length - (4 * i + 3)) + ',' + num.substring(num.length - (4 * i + 3));
-      //   if (cent > 0) {
-      //     if (sign == true) {
-      //       return (((sign) ? '' : '-') + num + '.' + cents);
-      //     } else if (sign == false) {
-      //       return '0.00'
-      //     }
-      //   } else {
-      //     return (((sign) ? '' : '-') + num);
-      //   }
-      // },
       //回退:back、拒绝:refuse
       coverFn(flag) {
         switch (flag) {
@@ -666,10 +639,7 @@
               this.fbalance2 = Number(res.data.fbalance).toLocaleString() + '.00'
             }
           } else if (res.statusCode == '700') {
-            this.$message({
-              message: res.msg,
-              type: 'error'
-            });
+            this._error(res.msg);
           }
         });
         this.creditPeriod(); //授信期限
