0d0b5d150ca7616c47b34760900cb7404b34f974
DESKTOP-NE5OB52\WangShimeng <wangshimeng@nuoyuan.com>
2018-12-07 11:16:15 +0800

    跳转登录页

--- src/util/http.js
+++ src/util/http.js
@@ -16,47 +16,47 @@ axios.defaults.baseURL = appConstant.baseUrl_common;
 
 axios.defaults.withCredentials = true;
 
-axios.interceptors.response.use(  response => {    
+axios.interceptors.response.use(response => {
   if (response.data && response.data.statusCode == 900) {
-    // window.location.href = platforUrl + "#/";  
-  };    
-  return response;  
-},   error => {
-  return Promise.reject(error);  
+    window.location.href = platforUrl + "#/";
+  };
+  return response;
+}, error => {
+  return Promise.reject(error);
 });
 
 export default {
   install(Vue, options) {
     Vue.prototype.get = function (url, params = {}) {
-        return new Promise((resolve, reject) => {    
-          axios.get(url, {      
-            params: params    
-          })    .then(response => {      
-            resolve(response.data);    
-          })    .catch(err => {      
-            reject(err)   
-          })  
+        return new Promise((resolve, reject) => {
+          axios.get(url, {
+            params: params
+          }).then(response => {
+            resolve(response.data);
+          }).catch(err => {
+            reject(err)
+          })
         })
       },
 
       Vue.prototype.post = function (url, data = {}) {
-        return new Promise((resolve, reject) => {    
-          axios.post(url, data)      
-            .then(response => {        
-              resolve(response.data);      
-            }, err => {        
-              reject(err);      
+        return new Promise((resolve, reject) => {
+          axios.post(url, data)
+            .then(response => {
+              resolve(response.data);
+            }, err => {
+              reject(err);
             })
             .catch((error) => {})
         })
       },
       Vue.prototype.Delete = function (url, data = {}) {
-        return new Promise((resolve, reject) => {    
-          axios.delete(url, data)      
-            .then(response => {        
-              resolve(response.data);      
-            }, err => {        
-              reject(err);      
+        return new Promise((resolve, reject) => {
+          axios.delete(url, data)
+            .then(response => {
+              resolve(response.data);
+            }, err => {
+              reject(err);
             })
             .catch((error) => {})
         })
